0e494e8898c004d2739c61d208bccbf396a305d5
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2022-01-12 18:42:27 -0300

    Criação dos Models (Contato e Categoria)

--- app_contatos/models.py
+++ app_contatos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from django.utils import timezone
 
-# Create your models here.
+class Categoria(models.Model):
+
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+
+    nome = models.CharField(max_length=255)
+    
+    sobrenome = models.CharField(max_length=255, blank=True)
+
+    telefone = models.CharField(max_length=20)
+
+    email = models.CharField(max_length=255, blank=True)
+
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    descricao = models.TextField(blank=True)
+
+    categoria = models.ForeignKey(Categoria, on_delete=DO_NOTHING)
