84f778fd9ebc428593b54432fbc5837233fd65e3
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2022-01-12 18:25:59 -0300

    Configurando base de dados para mysql

--- agenda/settings.py
+++ agenda/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_agenda',
+        'USER' : 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
